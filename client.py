bcef3eb61c88719c24c6342bac91047dbf380b9e
DeepP2667 <deeppatel2667@gmail.com>
2022-06-26 17:22:40 -0400

    Basic storing

--- client.py
+++ client.py
@@ -6,50 +6,118 @@ PORT = 21       # FTP Control Port
 class FtpClient():
 
     def __init__(self, client):
-       self.client = client
+       self.client = client                 # Control connection
+       self.logged = False                  # User logged or not
+       self.cmd = None                      # Command used
+       self.client_data_conn = None         # Data connection
        self.connect()
 
+    #---------------------------------#
+    ## FTP Connection/Authentication ##
+    #---------------------------------#
+
     def connect(self):
-        """
-        Connect user to Control Connection
-        """
+        # Connect user to Control Connection
         self.client.connect((HOST, PORT))
 
-    def start_control_conn(self):
-        """
-        Start Ccontrol Connection
-        """
-        welcome_msg = self.recv_message()
-        print(welcome_msg)
-        self.send_auth()
-
     def send_auth(self):
-        """
-        Send authorization information when server requests for it (username/password)
-        """
-        ack_username = self.recv_message()
+        # Send authorization information when server requests for it (username/password)
+        ack_username = self.recv_control_message()
         print(ack_username)
         username = input("").encode("utf-8")
         self.client.sendall(username)
 
-        ack_password = self.recv_message()
+        ack_password = self.recv_control_message()
         print(ack_password)
         password = input("").encode("utf-8")
         self.client.sendall(password)
 
-        access = self.recv_message()
-        print(access)
+        access = self.recv_control_message()
+        print(access[4:])
 
-    def recv_message(self):
-        """
-        Quick function to recieve messages and convert to string
-        Mostly used for communcation messages, such as welcome and authorization
+        # Status 230 - User successfuly logged
+        # Status 430 - Invalid username/password
+        if access[:3] == "230":
+            return True
+        elif access[:3] == "430":
+            return False
 
-        Returns:
-            str: Message recieved from server
-        """
-        return client.recv(1024).decode("utf-8")
+    #--------------------------#
+    ## FTP Control Connection ##
+    #--------------------------#
+
+    def start_control_conn(self):
+        # Start Ccontrol Connection
+        welcome_msg = self.recv_control_message()
+        print(welcome_msg)
+
+        self.logged = self.send_auth() 
+        if not self.logged:
+            return
         
+        # Main loop to send commands
+        while True:
+            self.cmd = input("Enter command: ").strip().upper()
+            self.send_cmd()
+            if self.cmd == "STOR":
+                self.STOR()
+            elif self.cmd == "QUIT":
+                self.client.close()
+                break
+
+    def send_cmd(self):
+        # Send commands to server
+        self.client.sendall((self.cmd).encode("utf-8"))
+
+    def recv_control_message(self):
+        # Recieve basic communication messages from server
+        return client.recv(1024).decode("utf-8")
+
+
+    #-----------------------#
+    ## FTP Data Connection ##
+    #-----------------------#
+
+    def start_data_conn(self):
+        # When command is sent and data wants to be transfered
+        # Start a data connection socket and bind to same IP on port 20
+        self.client_data_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_data_conn.connect((HOST, 20))
+        print("Starting Data Connection")
+
+    def close_data_conn(self):
+        # When data transfer complete
+        # Close socket
+        print("Closing Data Connection")
+        self.client_data_conn.close()
+        
+    #----------------#
+    ## FTP Commands ##
+    #----------------#
+
+    def STOR(self):
+        # Command for storing file to server
+        self.start_data_conn()
+
+        # Send filename
+        filename = input("Filename: ")
+        self.client_data_conn.sendall(filename.encode("utf-8"))
+
+        # Open and read files. Send in batches of 1024 bytes
+        file = open(f"{filename}", "rb")
+        print("Sending")
+
+        data = file.read(1024)
+        while data:
+            self.client_data_conn.sendall(data)
+            data = file.read(1024)
+        
+        print("Finised sending")
+        # Send a flag for file completely read
+        self.client_data_conn.sendall(b"DONE")
+            
+        file.close()
+        self.close_data_conn()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     c = FtpClient(client)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 import os
 
 HOST = socket.gethostbyname(socket.gethostname())
@@ -8,47 +9,135 @@ curr_server_dir = os.path.abspath('./Server')
 admin_username = "Test"
 admin_password = "123"
 
+def logger(func, code, msg):
+    logmsg = time.strftime(f"%Y-%m-%d %H:%M:%S [{code}] {func}: {msg}")
+    print(logmsg)
+
 class FtpServer():
     def __init__(self, conn, addr):
         self.curr_server_dir = curr_server_dir
-        self.control_conn = conn
-        self.addr = addr
-        self.data_conn = None
+        self.control_conn = conn                    # Control connection
+        self.addr = addr                            # Control connection IP
 
+        self.data_sock = None                       # Data connection socket
+        self.data_conn = None                       # Data connection
+        self.data_addr = None                       # Data connection IP
+
+    #----------------------#
+    ## FTP Authentication ##
+    #----------------------#
+    
     def request_auth(self):
-        """
-        Checks if user username/password is correct
+        # Send commands to enter username/password
 
-        Returns:
-            Boolean: True if successfully authenticated, otherwise False
-        """
         self.control_conn.sendall(b"Enter username:")
         username = self.control_conn.recv(1024).decode("utf-8")
+
         self.control_conn.sendall(b"Enter Password:")
         password = self.control_conn.recv(1024).decode("utf-8")
 
+        # Check if username/password are correct
         if username == admin_username and password == admin_password:
-            self.control_conn.sendall(f"Greetings {username}".encode("utf-8"))
+            self.control_conn.sendall(f"230 Greetings {username}".encode("utf-8"))
+            logger("request_auth", 230, f"Client {username} has connected")
             return True
         else:
-            self.control_conn.sendall(f"Access Denied".encode("utf-8"))
+            self.control_conn.sendall(f"430 Incorrect username or password".encode("utf-8"))
+            logger("request_auth", 430, "Access denied")
             return False
-       
+
+    #--------------------------#
+    ## FTP Control Connection ##
+    #--------------------------#
+
     def start_control_conn(self):
-        """
-        Start Ccontrol Connection
-        """
-        self.control_conn.sendall(b"Welcome to the server!\n")
+        # Start control connection
+        self.send_control_msg("Welcome to the server!\n")
+
+        # Check if user is authenticated
         accepted = self.request_auth()
         if not accepted:
             return
 
+        # Main loop to listen for commands
+        while True:
+            cmd = self.recv_control_msg()
+            if cmd == "STOR":
+                self.STOR()
+            elif cmd == "QUIT":
+                self.QUIT()
+                break
+            else:
+                logger("start_control_conn", 502, "Invalid command")
+                break
+
+    def send_control_msg(self, msg):
+        # Sending control messages
+        self.control_conn.sendall(msg.encode("utf-8"))
+
+    def recv_control_msg(self):
+        # Recieving commands from user
+        return self.control_conn.recv(1024).decode("utf-8")
+
+    #-----------------------#
+    ## FTP Data Connection ##
+    #-----------------------#
+
+    def start_data_conn(self):
+        # When user uses a command to transfer data
+        # Start a new socket and bind to same IP on port 20
+        self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.data_sock.bind((HOST, 20))
+        self.data_sock.listen(1)
+        self.data_conn, self.data_addr = self.data_sock.accept()
+        logger("start_control_conn", 225, "Data connection open")
+
+    def close_data_conn(self):
+        # When data transfer is finished
+        # Close socket
+        logger("close_data_conn", 226, "Data connection closed")
+        self.data_addr = None
+        self.data_conn.close()
+
+    #----------------#
+    ## FTP Commands ##
+    #----------------#
+
+    def QUIT(self):
+        # User disconnects from server
+        # Close control connection socket  
+        logger("QUIT", 426, "Client has disconnected")
+        self.control_conn.close()
+        self.addr = None
+
+    def STOR(self):
+        # Stores file into server
+        # Start data connection
+        self.start_data_conn()
+
+        # Request for file name and open file in write binary
+        filename = self.data_conn.recv(1024).decode("utf-8")
+        up_file = open(f"Server/{filename}", "wb")  
+        
+        # Keep writing data until user finishes sending file
+        # [FLAG] - DONE - File completely sent by user
+        while True:
+            data = self.data_conn.recv(1024)
+            if data == b"DONE":
+                break
+            up_file.write(data)
+        
+        logger("STOR", 250, "File stored successfully")
+        up_file.close()
+        self.close_data_conn()
+
 def start_listening():
     # Socket listening over Ipv4 using TCP
+    # Control connection socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(1)
-    print("Server is listening...")
+    logger("start_listening", 220, "Server started for user")
     
     while True:
         conn, addr = sock.accept()  
